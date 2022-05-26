61cfce75103bb5e446f6d36d241c3852851ae4b0
rajveerpanchal <87427859+rajveerpanchal@users.noreply.github.com>
2022-05-26 17:35:31 +0530

    first commit

---
+++ src/app/MyComponent/todos/todos.component.ts
@@ -0,0 +1,44 @@
+import { not } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from "../../Todo";
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  localItem: string | null;
+  todos:Todo[];
+  constructor() { 
+    this.localItem = localStorage.getItem("todos");
+    if(this.localItem == null){
+    this.todos = [];
+    }
+    else{ 
+      this.todos = JSON.parse(this.localItem); 
+    }
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  deleteTodo(todo:Todo){
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index ,1)
+    localStorage.setItem("todos" , JSON.stringify(this.todos));
+  }
+  addTodo(todo:Todo){
+    console.log(todo);
+    localStorage.setItem("todos" , JSON.stringify(this.todos));
+    this.todos.push(todo);
+  }
+  toggleTodo(todo:Todo){
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active=!this.todos[index].active;
+    localStorage.setItem("todos" , JSON.stringify(this.todos));
+    
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  constructor(){
+  //   setTimeout(() => {
+  //   this.title = "i am Rajveer !!!";
+  // },2000);
+
+  }
+
 }
